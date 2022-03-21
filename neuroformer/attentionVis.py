3499569cbfa988f45666764e6fa4dacce1718de9
Antonis Antoniades <antonis@Antoniss-MacBook-Pro.local>
2022-03-21 12:41:38 -0700

    implemented ViT attention grad rollout

--- neuroformer/attentionVis.py
+++ neuroformer/attentionVis.py
@@ -12,6 +12,77 @@ from numpy import linalg as LA
 from SpikeVidUtils import get_frame_idx
 from utils import top_k_top_p_filtering
 
+
+def grad_rollout(attentions, gradients, discard_ratio):
+    result = torch.eye(attentions[0].size(-1))
+    with torch.no_grad():
+        for attention, grad in zip(attentions, gradients):                
+            weights = grad
+            attention_heads_fused = (attention*weights).mean(axis=1)
+            attention_heads_fused[attention_heads_fused < 0] = 0
+
+            # Drop the lowest attentions, but
+            # don't drop the class token
+            flat = attention_heads_fused.view(attention_heads_fused.size(0), -1)
+            _, indices = flat.topk(int(flat.size(-1)*discard_ratio), -1, False)
+            #indices = indices[indices != 0]
+            flat[0, indices] = 0
+
+            I = torch.eye(attention_heads_fused.size(-1))
+            a = (attention_heads_fused + 1.0*I)/2
+            a = a / a.sum(dim=-1)
+            result = torch.matmul(a, result)
+    
+    # Look at the total attention between the class token,
+    # and the image patches
+    mask = result[0, 0 , 1 :]
+    # In case of 224x224 image, this brings us from 196 to 14
+    width = int(mask.size(-1)**0.5)
+    mask = mask.reshape(width, width).numpy()
+    mask = mask / np.max(mask)
+    return mask    
+
+class VITAttentionGradRollout:
+        """
+        This class is an adaptation of Jacob Gildenblat's implementation: 
+        https://github.com/jacobgil/vit-explain
+
+        We calculate Attention Rollou (Abnar, Zuidema, 2020), 
+        for stimuluts-state attention, and condition
+        it on the gradient of a specific target neuron.
+
+        This way we can get neuron-specific attentions.
+        """
+
+        def __init__(self, model, module, attn_layer_name='attn_drop', discard_ratio=0.8):
+                self.model = model
+                self.module = module
+                self.discard_ration = discard_ratio
+                for name, module in self.module.named_modules():
+                        if attn_layer_name in name:
+                                module.register_forward_hook(self.get_attention)
+                                module.register_backward_hook(self.get_attention_gradient)
+                
+                self.attentions = []
+                self.attention_gradients = []
+
+        def get_attention(self, module, input, output):
+                self.attentions.append(output.cpu())
+        
+        def get_attention_gradient(self, module, grad_input, grad_output):
+                self.attention_gradients.append(grad_input[0].cpu())
+
+        def __call__(self, input_tensor, category_index):
+                self.model.zero_grad()
+                output = self.model(input_tensor)
+                category_mask = torch.zeros(output.size())
+                category_mask[:, category_index] = 1
+                loss = (output * category_mask).sum()
+                loss.backward()
+
+                return grad_rollout(self.attentions, self.attention_gradients, self.discard_ratio)
+
+
 class AttentionVis:
         '''attention Visualizer'''
         
