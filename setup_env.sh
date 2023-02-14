4c1bb74e342a4ff140d0d05e1a1e52a2c612246c
woanderer <antonis.a@icloud.com>
2023-02-13 16:57:33 -0800

    file to setup enviornment

---
+++ setup_env.sh
@@ -0,0 +1,13 @@
+conda create -n neuroformer python=3.9.15
+conda activate neuroformer
+conda install -c conda-forge jupyterlab
+conda install -c anaconda scipy 
+conda install -c conda-forge sk-video
+conda install -c conda-forge scikit-image
+conda install -c conda-forge matplotlib
+conda install -c conda-forge tqdm
+conda install -c conda-forge torchmetrics 
+conda install -c conda-forge einops
+conda install pandas
+conda install -c conda-forge omegaconf
+conda install -c conda-forge timm 
